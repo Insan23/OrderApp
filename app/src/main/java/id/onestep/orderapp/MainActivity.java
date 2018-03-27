052b0bf188c08a542dec49ed176260c041b33a11
Insan23 <green.blaze235@gmail.com>
2018-03-27 21:02:54 +0700

    bagian 1 event handling klik button

--- app/src/main/java/id/onestep/orderapp/MainActivity.java
+++ app/src/main/java/id/onestep/orderapp/MainActivity.java
@@ -15,94 +15,19 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    CheckBox cbNasi, cbMie, cbEsteh, cbEsjeruk;
-    TextView txtOrder;
-    EditText etNama, etJumlahNasi, etJumlahMie, etJumlahEsteh, etJumlahEsjeruk;
-    Button submit;
-    //int jumlahNasi,jumlahMie,jumlahEsteh,jumlahEsjeruk;
-    String order;
-    double totalHarga;
-    boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cbNasi = (CheckBox) findViewById(R.id.ckNasi);
-        cbMie = (CheckBox) findViewById(R.id.ckMie);
-        cbEsteh = (CheckBox) findViewById(R.id.ckEsteh);
-        cbEsjeruk = (CheckBox) findViewById(R.id.ckEsjeruk);
-        txtOrder = (TextView) findViewById(R.id.orderSummary);
-        etNama = (EditText) findViewById(R.id.etNama);
-        etJumlahNasi = (EditText) findViewById(R.id.etJumlahNasi);
-        etJumlahMie = (EditText) findViewById(R.id.etJumlahMie);
-        etJumlahEsteh = (EditText) findViewById(R.id.etEsteh);
-        etJumlahEsjeruk = (EditText) findViewById(R.id.etEsjeruk);
+    }
 
-        Locale localeID = new Locale("in", "ID");
-        final NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+    public void submitOrder(View view) {
+        tampilkan(123456789);
+    }
 
-        submit = (Button) findViewById(R.id.btnSubmit);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                order = "";
-                if (cbNasi.isChecked()) {
-                    order += "Nasi Goreng\n";
-                    if (etJumlahNasi.getText().length() == 0) {
-                        etJumlahNasi.setError("harus diisi");
-                        status = false;
-                    } else {
-                        status = true;
-                        int jumlahNasi = Integer.parseInt(etJumlahNasi.getText().toString());
-                        totalHarga += jumlahNasi * 7000;
-                    }
-                }
-                if (cbMie.isChecked()) {
-                    if (etJumlahMie.getText().length() == 0) {
-                        etJumlahMie.setError("harus diisi");
-                        status = false;
-                    } else {
-                        order += "Mie Goreng\n";
-                        int jumlahMie = Integer.parseInt(etJumlahMie.getText().toString());
-                        totalHarga += jumlahMie * 7000;
-                    }
-                }
-                if (cbEsteh.isChecked()) {
-                    order += "Es Teh\n";
-                    if (etJumlahEsteh.getText().length() == 0) {
-                        etJumlahEsteh.setError("harus diisi");
-                        status = false;
-                    } else {
-                        status = true;
-
-                    }
-                    int jumlahEsteh = Integer.parseInt(etJumlahEsteh.getText().toString());
-                    totalHarga += jumlahEsteh * 3000;
-                }
-                if (cbEsjeruk.isChecked()) {
-                    order += "Es Jeruk\n";
-                    if (etJumlahEsjeruk.getText().length() == 0) {
-                        etJumlahEsjeruk.setError("harus diisi");
-                        status = false;
-                    } else {
-                        status = true;
-
-                    }
-                    int jumlahEsjeruk = Integer.parseInt(etJumlahEsjeruk.getText().toString());
-                    totalHarga += jumlahEsjeruk * 4000;
-                }
-
-                order += "\nharga : " + formatRupiah.format((double) totalHarga);
-                txtOrder.setText(order);
-                Intent i = new Intent(Intent.ACTION_SENDTO);
-                i.setData(Uri.parse("mailto:"));
-                i.putExtra(Intent.EXTRA_SUBJECT, "order dari " + etNama.getText().toString());
-                i.putExtra(Intent.EXTRA_TEXT, order);
-                if (status) {
-                    startActivity(i);
-                }
-            }
-        });
+    public void tampilkan(int angka) {
+        TextView quantity = findViewById(R.id.quantity_text);
+        quantity.setText(angka);
     }
 }
